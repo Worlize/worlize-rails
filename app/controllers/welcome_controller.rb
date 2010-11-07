2a203d907e29685fee7bb1f31b51aa257919089f
Brian McKelvey <brian@worlize.com>
2010-11-07 01:49:56 -0700

    First progress implementing new site design.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,6 +3,19 @@ class WelcomeController < ApplicationController
   before_filter :require_user, :only => [:enter]
   
   def index
+    @user_session = UserSession.new
+    characters = {
+      'halbert' => 'I am HAL(bert).  Identify yourself!',
+      'gus' => 'They call me Guss... What do they call you?',
+      'jules' => 'I\'m jules... what\'s YOUR name?',
+      'neo' => 'Neo is the name... Are you plugged in?',
+      'sam' => 'sam, I am... and you?',
+      'sunny' => 'I\'m sunny... with a chance of rain.'
+    }
+    
+    character_names = characters.keys
+    @character_name = character_names[rand(character_names.length)]
+    @saying = characters[@character_name]
   end
   
   def enter
---
+++ public/javascripts/script.js
@@ -0,0 +1,32 @@
+/* Author: 
+
+*/
+
+
+
+
+jQuery('document').ready(function() {
+  $('#homepage_login_link').click(function(event) {
+    event.preventDefault();
+    $('#homepage_login_layer').fadeIn();
+    $('#user_session_username').focus();
+  });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
