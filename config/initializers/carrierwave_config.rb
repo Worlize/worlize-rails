c4bf8f9f861009d04af021505a5afed4283e14c2
Brian McKelvey <brian@worlize.com>
2010-11-11 18:52:51 -0800

    Updating carrierwave configuration to load in the Worlize configuration yml file if it's not already loaded.

--- config/initializers/carrierwave_config.rb
+++ config/initializers/carrierwave_config.rb
@@ -1,3 +1,7 @@
+if Worlize.config.nil?
+  require 'application_config.rb'
+end
+
 CarrierWave.configure do |config|
   config.s3_access_key_id = Worlize.config['amazon']['access_key_id']
   config.s3_secret_access_key = Worlize.config['amazon']['secret_access_key']
