a8977fa8c6ebb59bdcac9241e4b497d92797d984
Brian McKelvey <brian@worlize.com>
2010-11-27 00:52:09 -0800

    Updating asset_host to just worlize.com since I dont have a certificate for static.worlize.com

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -38,7 +38,7 @@ Worlize::Application.configure do
   config.action_mailer.delivery_method = :sendmail
 
   # Enable serving of images, stylesheets, and javascripts from an asset server
-  config.action_controller.asset_host = "https://static.worlize.com"
+  config.action_controller.asset_host = "https://worlize.com"
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
