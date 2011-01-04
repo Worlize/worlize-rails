baceb24e1bf6b2fc9e47a30a36ce6a2e57ba2065
Brian McKelvey <brian@worlize.com>
2011-01-03 23:14:24 -0800

    Fixing a bug.. forgot to specify the :class_name property on an ActiveRecord relationship declaration.

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,7 +11,7 @@ class User < ActiveRecord::Base
   has_many :avatar_instances, :dependent => :nullify
   has_many :prop_instances, :dependent => :nullify
   
-  has_many :created_avatars, :foreign_key => 'creator_id', :dependent => :nullify
+  has_many :created_avatars, :foreign_key => 'creator_id', :dependent => :nullify, :class_name => 'Avatar'
   
   has_many :received_gifts, :class_name => 'Gift', :foreign_key => 'recipient_id'
   has_many :sent_gifts,     :class_name => 'Gift', :foreign_key => 'sender_id'
