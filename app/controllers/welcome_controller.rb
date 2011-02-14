87e119a5331bf2391a98eb135402769278e7d051
Brian McKelvey <brian@worlize.com>
2011-02-13 21:24:37 -0800

    Adding ability to switch off the marketplace links with a config setting.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -28,13 +28,14 @@ class WelcomeController < ApplicationController
   
   def enter
     @configJSON = Yajl::Encoder.encode({
-      "user_guid" => current_user.guid,
-      "username" => current_user.username,
-      "admin" => current_user.admin?,
-      "interactivity_session" => current_user.interactivity_session.serializable_hash,
-      "authenticity_token" => form_authenticity_token,
-      "cookies" => cookies,
-      "remaining_invites" => current_user.invites
+      'marketplace_enabled' => Worlize.config['marketplace_enabled'],
+      'user_guid' => current_user.guid,
+      'username' => current_user.username,
+      'admin' => current_user.admin?,
+      'interactivity_session' => current_user.interactivity_session.serializable_hash,
+      'authenticity_token' => form_authenticity_token,
+      'cookies' => cookies,
+      'remaining_invites' => current_user.invites
     })
     render :enter, :layout => false
   end
