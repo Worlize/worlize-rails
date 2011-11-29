75141311e672e391d4d49367583141962ff07423
Brian McKelvey <brian@worlize.com>
2011-11-29 11:30:14 -0800

    Updating status page display to accomodate new interactivity server status schema.

--- app/controllers/admin/status_controller.rb
+++ app/controllers/admin/status_controller.rb
@@ -4,16 +4,19 @@ class Admin::StatusController < ApplicationController
 
   def show
     @total_user_count = 0
-    @total_session_count = 0
     @total_room_count = 0
     
     server_manager = Worlize::InteractServerManager.instance
-    redis = Worlize::RedisConnectionPool.get_client(:presence)
+    redis = Worlize::RedisConnectionPool.get_client(:room_server_assignments)
     @interactivity_servers = server_manager.active_servers
+    
+    @interactivity_servers.sort! do |a,b|
+      a['server_id'].downcase <=> b['server_id'].downcase
+    end
+    
     @interactivity_servers.each do |server|
       server['room_count'] = redis.scard("roomsOnServer:#{server['server_id']}")
       @total_user_count += server['user_count']
-      @total_session_count += server['session_count']
       @total_room_count += server['room_count']
     end
     
