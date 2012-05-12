0da0da16192fef11d65bbaccfc89f50dd8a4e6be
Brian McKelvey <brian@worlize.com>
2012-05-12 15:09:18 -0700

    Invalidating existing sessions.

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random, 
 # no regular words or you'll be exposed to dictionary attacks.
-Rails.application.config.secret_token = 'a64f7eb8d352eee0ef2823070288349017ed24e0828be93d5d2afc8a044b1966b90b32f185dd57175035b966422a3dd27f65274746330f9fe0e57b2ee341c69a'
+Rails.application.config.secret_token = 'a64f7eb8d352eee0ef2823070288349017ed24e0828be93d5d2afc8a044b1966b90b32f185dd57175035b966422a3dd27f65274746330f9fe0e57b2ee341c69b'
